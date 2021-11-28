7515793f1a5cbce878f42bb8ff22bf19806670fd
xchmel33 <62353125+xchmel33@users.noreply.github.com>
2021-11-28 16:39:37 +0100

    x

--- App/Controller/licidator.php
+++ App/Controller/licidator.php
@@ -5,10 +5,10 @@ class licidator extends controller
 {
     public function index(){
         if (isset($_POST['approve'])){
-
+            $this->db->update('auction',['organizator_id','status'],[$_SESSION['user_id'],'started'],'auction_id',$_POST['auction_id']);
         }
         if (isset($_POST['disapprove'])){
-
+            $this->db->update('auction',['organizator_id','status'],[-1,'disapproved'],'auction_id',$_POST['auction_id']);
         }
         if (isset($_POST['setT'])){
 
@@ -17,10 +17,10 @@ class licidator extends controller
 
         }
         if (isset($_POST['confirm'])){
-
+            $this->db->update('auction_user',['user_approved'],[1],'auction_id',$_POST['auction_id']);
         }
         if (isset($_POST['reject'])){
-
+            $this->db->update('auction_user',['user_approved'],[-1],'auction_id',$_POST['auction_id']);
         }
 
         $data['title'] = 'licidator';
--- App/View/Template/licidator.php
+++ App/View/Template/licidator.php
@@ -29,15 +29,16 @@
     <?php
     foreach ($data['auctions'] as $auction){
         $auction_owner = $this->db->selectByColumnName('user','user_id',$auction['owner_id'],'username')[0]['username'];
-        if ($auction['organisator_id'] != $_SESSION['user_id']) continue;
+        if ($auction['organizator_id'] != $_SESSION['user_id']) continue;
         $auction_raise_size = $auction['highest_bid']*0.1;
-        $request_join_users = $this->db->selectAll(['user','auction_user'],'user_id');
+        $auction_user = $this->db->selectAll(['user','auction_user'],'user_id');
         echo '
-            <div class="info"><div class="user_join_wrapper"><h4>USERS WANTING TO JOIN:</h4><form class="users_join">';
-        foreach ($request_join_users as $rj_user){
-            if ($rj_user['user_approved']) continue;
+            <div class="info"><div class="user_join_wrapper"><h4>USERS WANTING TO JOIN:</h4><form method="post" action="licidator" class="users_join">';
+        foreach ($auction_user as $au_user){
+            if ($au_user['user_approved'] != 0 || $au_user['auction_id'] != $auction['auction_id']) continue;
             echo '
-                <label>'.$rj_user['username'].'</label>
+                <label>'.$au_user['username'].'</label>
+                <input hidden name="auction_id" value="'.$auction["auction_id"].'">
                 <input type="submit" name="confirm" value="confirm join">
                 <input type="submit" name="reject" value="reject join">
                 ';
@@ -55,25 +56,9 @@
                         <input hidden name="auction_id" value="'.$auction["auction_id"].'">
                         <input type="number" size="'.$auction_raise_size.'" name="bidC_val" value="'.$auction_raise_size.'">
                         <input type="submit" name="bidR" value="SET MIN REQUIRED BID"><br>
-                        <div id="auction_timeout'.$auction['auction_id'].'">
-                            <input id="tD" class="short" type="number" name="timeoutD">d
-                            <input id="tH" class="short" type="number" name="timeoutH">h
-                            <input id="tM" class="short" type="number" name="timeoutM">m
-                            <input id="tS" class="short" type="number" name="timeoutS">s
-                            <input type="submit" name="setT" value="SET TIMELEFT"><br>
-                            <input type="submit" name="endA" value="END AUCTION">
-                        </div>
-                        <script type="text/javascript">
-                            setTimeout(function (){
-                                let countDownDate = new Date("' . $auction['end_time'] . '").getTime();
-                                let now = new Date().getTime();
-                                let distance = countDownDate - now;
-                                document.getElementById("tD").value = Math.floor(distance / (1000 * 60 * 60 * 24));
-                                document.getElementById("tH").value = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                                document.getElementById("tM").value = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                                document.getElementById("tS").value = Math.floor((distance % (1000 * 60)) / 1000);
-                            },1000);
-                        </script>
+                        <input name="setDate" type="date" value="'.explode(' ',$auction['end_time'])[0].'">
+                        <input name="setTime" type="time" value="'.explode(' ',$auction['end_time'])[1].'">
+                        <input type="submit" name="setT" value="SET TIMELEFT"><br>
                     </form>
                 </ul>
             </div>';
